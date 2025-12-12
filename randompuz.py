bbc9d9451b16c4c7fb0663b2e86fc81ab9c41530
Ben Armstrong <synrg@debian.org>
2025-12-12 16:29:06 -0400

    randompuz: Tidy up main program

--- randompuz.py
+++ randompuz.py
@@ -31,9 +31,9 @@ def get_latest_image_number(session):
     latest_image_number = int(mat[0])
     return latest_image_number
 
-def get_random_image_url(low, high):
-    random_image_number = random.randint(low, high)
-    return f'{BASE_URL}/browse/image/{random_image_number}'
+def choose_random_image_url(low, high):
+    image_number = random.randint(low, high)
+    return f'{BASE_URL}/browse/image/{image_number}'
 
 def get_params(session):
     lowest = 0
@@ -67,34 +67,40 @@ def get_params(session):
         sys.exit(f"Lowest image number {lowest} must be less than or equal to highest image number {highest}.")
     return (lowest, highest)
 
-session = requests_cache.CachedSession(CACHE_FILE, expire_after=timedelta(days=1))
-lowest_image_number, highest_image_number = get_params(session)
-
-errors = 0
-urls_tried = []
-random_image_url = None
-size_of_range = highest_image_number - lowest_image_number + 1
-max_errors = min(MAX_ERRORS, size_of_range)
-while not random_image_url and errors < max_errors and len(urls_tried) < size_of_range:
-    url = get_random_image_url(lowest_image_number, highest_image_number)
-    if url not in urls_tried:
-        response = session.get(url, timeout=TIMEOUT)
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.text, features='html.parser')
-            main_image = soup.select_one('img.main-image')
-            if main_image:
-                random_image_url = url
+def get_random_image_url(session, lowest, highest):
+    errors = 0
+    urls_tried = []
+    valid_url = None
+    size_of_range = highest - lowest + 1
+    max_errors = min(MAX_ERRORS, size_of_range)
+    while not valid_url and errors < max_errors and len(urls_tried) < size_of_range:
+        url = choose_random_image_url(lowest, highest)
+        if url not in urls_tried:
+            response = session.get(url, timeout=TIMEOUT)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, features='html.parser')
+                main_image = soup.select_one('img.main-image')
+                if main_image:
+                    valid_url = url
+                else:
+                    print(f"No image on page; discarding: {url}")
+                    urls_tried.append(url)
+                    errors += 1
             else:
-                print(f"No image on page; discarding: {url}")
+                print(f"Image not found; discarding: {url}")
                 urls_tried.append(url)
                 errors += 1
         else:
-            print(f"Image not found; discarding: {url}")
-            urls_tried.append(url)
-            errors += 1
-    else:
-        print(f"Not requesting URL again: {url}")
-if not random_image_url:
-    sys.exit(f"Gave up after {errors} retries. Site down or no valid images in range: {lowest_image_number}-{highest_image_number}")
+            print(f"Not requesting URL again: {url}")
+    if not valid_url:
+        sys.exit(f"Gave up after {errors} retries. Site down or no valid images in range: {lowest}-{highest}")
+    return valid_url
 
-print(f"\nRandom puzzle image: {random_image_url}\n")
+def main():
+    session = requests_cache.CachedSession(CACHE_FILE, expire_after=timedelta(days=1))
+    lowest, highest = get_params(session)
+    url = get_random_image_url(session, lowest, highest)
+    print(f"\nRandom puzzle image: {url}\n")
+
+if __name__ == "__main__":
+    main()
